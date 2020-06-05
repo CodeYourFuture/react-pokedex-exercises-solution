62aa5b12fd7e6e5b7d1fc415b328fbf73fde9853
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-06-05 11:03:39 +0100

    Week 1 Exercise G completed

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ function App() {
     <div>
       <Logo />
       <BestPokemon />
+      <CaughtPokemon />
     </div>
   );
 }
 
 const Logo = () => {
+  const appName = "Pokedex";
+
   return (
     <header>
-      <h1>Welcome to the Pokedex</h1>
+      <h1>Welcome to the {appName}</h1>
       <img
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
         alt="Pokedex"
@@ -22,7 +25,24 @@ const Logo = () => {
 };
 
 const BestPokemon = () => {
-  return <p>My favourite Pokemon is Squirtle</p>;
+  const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+
+  return (
+    <div>
+      <p>My favourite Pokemon is Squirtle</p>
+      <ul>
+        {abilities.map((ability, index) => {
+          return <li key={index}>{ability}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
+
+function CaughtPokemon() {
+  const date = new Date().toLocaleDateString();
+
+  return <p>Caught 0 Pokemon on {date}</p>;
+}
 
 export default App;
