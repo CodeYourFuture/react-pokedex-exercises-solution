2375a575e6080b51ee695eb15d3561b08a076a64
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-06-05 10:55:08 +0100

    Week 1 Exercise D completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Welcome to the Pokedex</h1>
+        <img
+          src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+          alt="Pokedex"
+        />
       </header>
     </div>
   );
