8547f912438af1adff7540e855e19fbd0569fbc1
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-06-05 10:56:43 +0100

    Week 1 Exercise E completed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import React from "react";
 function App() {
   return (
     <div>
-      <header>
-        <h1>Welcome to the Pokedex</h1>
-        <img
-          src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
-          alt="Pokedex"
-        />
-      </header>
+      <Logo />
+      <BestPokemon />
     </div>
   );
+}
+
+function Logo() {
+  return (
+    <header>
+      <h1>Welcome to the Pokedex</h1>
+      <img
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+        alt="Pokedex"
+      />
+    </header>
+  );
+}
+
+function BestPokemon() {
+  return <p>My favourite Pokemon is Squirtle</p>;
 }
 
 export default App;
