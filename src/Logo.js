31656da6f4499a714f2b22728145560401243202
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-06-05 11:11:45 +0100

    Week 2 Exercise A completed

--- src/Logo.js
+++ src/Logo.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Logo = (props) => {
+  function logWhenClicked() {
+    console.log("Clicked!");
+  }
+
   return (
     <header>
       <h1>Welcome to the {props.appName}</h1>
       <img
+        onClick={logWhenClicked}
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
         alt="Pokedex"
       />
